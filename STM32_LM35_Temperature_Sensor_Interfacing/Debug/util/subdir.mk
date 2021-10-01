250aa93fbd23153947a61f8a34f903d8b6d5f3f2
Aleksandr Uzienko <a.uzienko@gmail.com>
2021-10-01 18:32:22 +0300

    STM32 test LM35

---
+++ STM32_LM35_Temperature_Sensor_Interfacing/Debug/util/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../util/DWT_Delay.c 
+
+OBJS += \
+./util/DWT_Delay.o 
+
+C_DEPS += \
+./util/DWT_Delay.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+util/%.o: ../util/%.c util/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I../util -I../ECUAL/LM35 -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
