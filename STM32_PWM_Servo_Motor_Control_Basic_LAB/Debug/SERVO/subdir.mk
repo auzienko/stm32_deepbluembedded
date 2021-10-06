fe792873ce78b59070cc468f97374ec502225adc
Aleksandr Uzienko <a.uzienko@gmail.com>
2021-10-06 20:11:03 +0300

    Basic Servo Motor Test

---
+++ STM32_PWM_Servo_Motor_Control_Basic_LAB/Debug/SERVO/subdir.mk
@@ -0,0 +1,23 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../SERVO/SERVO.c \
+../SERVO/SERVO_cfg.c 
+
+OBJS += \
+./SERVO/SERVO.o \
+./SERVO/SERVO_cfg.o 
+
+C_DEPS += \
+./SERVO/SERVO.d \
+./SERVO/SERVO_cfg.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+SERVO/%.o: ../SERVO/%.c SERVO/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I../SERVO -I../util -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
